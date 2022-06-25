4a7988369b4c4db694d108072d4c386517f866b7
ragsav <raghav227996399@gmail.com>
2022-06-25 14:23:48 +0530

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hashText, setHashText] = useState("");
+  const [logs, setLogs] = useState("");
+
+  const onSubmit = () => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar"></div>
+      <div className="container-main">
+        <form className="hash-input-form">
+          <div className="mb-3 w-100 d-flex flex-column align-items-start justify-content-start">
+            <label for="exampleFormControlTextarea1" className="form-label">
+              Enter Hashes
+            </label>
+            <textarea
+              value={hashText}
+              onChange={(e) => {
+                setHashText(e.target.value);
+              }}
+              className="form-control"
+              id="hashes"
+              rows="10"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            {isLoading ? "Processing..." : "Submit"}
+          </button>
+        </form>
+        <div className="mb-3 w-100 mt-5 d-flex flex-column align-items-start justify-content-start">
+          <label className="form-label">Logs</label>
+          <textarea className="form-control" rows="20" readOnly value={logs}></textarea>
+        </div>
+      </div>
     </div>
   );
 }
